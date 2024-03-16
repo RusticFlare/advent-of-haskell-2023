0392d8ea8b6207290b14c45779ffda673dd5312b
rusticflare <j_d_baker@hotmail.com>
2024-03-16 12:43:59 +0000

    Tidy

--- app/Day02.hs
+++ app/Day02.hs
@@ -3,12 +3,15 @@ module Day02 where
 import qualified Data.Map as Map
 import Text.ParserCombinators.Parsec
 import Parser
+import Data.Function((&))
+import Data.Foldable.Extra (sumOn')
 
 day02 :: IO ()
 day02 = do
    input <- readFile "inputs/Day02.txt"
-   print $ day02Part1 input
-   print $ day02Part2 input
+   let games = map parseGame $ lines input
+   print $ "Part 1: " ++ show (day02Part1 games)
+   print $ "Part 2: " ++ show (day02Part2 games)
 
 data Colour = Red | Green | Blue deriving (Enum, Eq, Ord, Show)
 
@@ -17,15 +20,15 @@ newtype Round = Round { balls :: Map.Map Colour Integer } deriving (Show)
 data Game = Game { gameNumber :: Integer
                 , rounds :: [Round] } deriving (Show)
 
-day02Part1 :: String -> Integer
-day02Part1 input = sum $ map (score . parseGame) $ lines input
+day02Part1 :: [Game] -> Integer
+day02Part1 = sumOn' score
 
-day02Part2 :: String -> Integer
-day02Part2 input = sum $ map (power . parseGame) $ lines input
+day02Part2 :: [Game] -> Integer
+day02Part2 = sumOn' power
 
 parseGame :: String -> Game
 parseGame line = case parse gameParser "" line of
-    Right result ->  result
+    Right result -> result
     e -> error $ show e
 
 gameParser :: Parser Game
@@ -35,25 +38,24 @@ roundParser :: Parser Round
 roundParser = Round . Map.fromList <$> commaSep ballCountParser
 
 ballCountParser :: Parser (Colour, Integer)
-ballCountParser = reverseTuple <$> natural <*> colourParser
-    where reverseTuple a b = (b, a)
+ballCountParser = flip (,) <$> natural <*> colourParser
 
 colourParser :: Parser Colour
 colourParser = (Blue <$ string "blue") <|> (Green <$ string "green") <|> (Red <$ string "red")
 
 score :: Game -> Integer
 score game
-    | gameIsPossible game = gameNumber game
+    | gameIsPossible game = game & gameNumber
     | otherwise = 0
 
 gameIsPossible :: Game -> Bool
 gameIsPossible game = all roundIsPossible $ rounds game
 
 roundIsPossible :: Round -> Bool
-roundIsPossible r = colourIsPossible Red 12 r && colourIsPossible Green 13 r && colourIsPossible Blue 14 r
+roundIsPossible r = all (uncurry (colourIsPossible r)) [(Red, 12), (Green, 13), (Blue, 14)]
 
-colourIsPossible :: Colour -> Integer -> Round -> Bool
-colourIsPossible colour maxCount r = Map.findWithDefault 0 colour (balls r) <= maxCount
+colourIsPossible :: Round -> Colour -> Integer -> Bool
+colourIsPossible r colour maxCount = Map.findWithDefault 0 colour (balls r) <= maxCount
 
 power :: Game -> Integer
 power game = product $ Map.elems $ foldl (Map.unionWith max) Map.empty $ map balls $ rounds game
