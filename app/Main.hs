9411732641be6fece894f0a162679a664fff4a40
rusticflare <j_d_baker@hotmail.com>
2024-03-18 20:10:21 +0000

    Day 3 Part 1 rough

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Day01 (day01)
 import Day02 (day02)
+import Day03 (day03)
 
 main :: IO ()
 main = do
@@ -12,4 +13,5 @@ runDay :: String -> IO ()
 runDay day = case read day :: Int of
     1 -> day01
     2 -> day02
+    3 -> day03
     _ -> error $ day ++ " is not a valid day"
