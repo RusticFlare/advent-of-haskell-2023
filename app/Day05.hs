d0bf818b5af7e97fed7749e1601f1ffb4b1f0785
rusticflare <j_d_baker@hotmail.com>
2024-04-14 10:34:31 +0100

    Day 5 Part 1

--- app/Day05.hs
+++ app/Day05.hs
@@ -2,6 +2,9 @@ module Day05 where
 
 import Text.ParserCombinators.Parsec
 import Parser
+import Control.Monad (msum)
+import Data.Maybe (fromMaybe)
+import Data.Foldable (Foldable(foldl'))
 
 day05 :: IO ()
 day05 = do
@@ -11,7 +14,7 @@ day05 = do
    print $ "Part 2: " ++ show (day05Part2 almanac)
 
 day05Part1 :: Almanac -> Integer
-day05Part1 _ = 0
+day05Part1 almanac = minimum $ locations almanac
 
 day05Part2 :: Almanac -> Integer
 day05Part2 _ = 0
@@ -25,11 +28,29 @@ data AlmanacMap = AlmanacMap { sourceCategory :: String
                              , destinationCategory :: String
                              , ranges :: [AlmanacRange] } deriving (Show)
 
-data AlmanacRange = AlmanacRange { destinationRangeStart :: Integer
-                                 , sourceRangeStart :: Integer
+data AlmanacRange = AlmanacRange { destinationStart :: Integer
+                                 , sourceStart :: Integer
                                  , rangeLength :: Integer } deriving (Show)
 
 -- Soloutions
+
+locations :: Almanac -> [Integer]
+locations almanac = map (seedLocation almanac) (seeds almanac)
+
+seedLocation :: Almanac -> Integer -> Integer
+seedLocation almanac seed = foldl' applyMap seed $ maps almanac
+
+applyMap :: Integer ->  AlmanacMap -> Integer
+applyMap input almanacMap = fromMaybe input $ msum appliedRanges
+    where appliedRanges = map (applyRange input) $ ranges almanacMap
+
+applyRange :: Integer -> AlmanacRange -> Maybe Integer
+applyRange input range
+    | input >= s && input < s + l = Just (d - s + input)
+    | otherwise = Nothing
+    where s = sourceStart range
+          l = rangeLength range
+          d = destinationStart range
 
 -- Parser
 
