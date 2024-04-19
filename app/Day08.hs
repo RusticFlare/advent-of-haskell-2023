bbc2cee38b8adb67a20166401ad2995bfae6d8af
rusticflare <j_d_baker@hotmail.com>
2024-04-19 23:35:08 +0100

    Use Char triples

--- app/Day08.hs
+++ app/Day08.hs
@@ -11,45 +11,50 @@ day08 = do
    print $ "Part 1: " ++ show (part1 documents)
    print $ "Part 2: " ++ show (part2 documents)
 
-part1 :: Documents -> Int
-part1 = steps "AAA"
+part1 :: Documents (Char, Char, Char) -> Int
+part1 = steps ('A', 'A', 'A')
 
 part2 :: a -> Integer
 part2 _ = 0
 
 -- Types
 
-data Documents = Documents { instuctions :: [Map.Map String (String, String) -> String -> String]
-                           , network :: Map.Map String (String, String) }
+data Documents a = Documents { instuctions :: [Map.Map a (a, a) -> a -> a]
+                             , network :: Map.Map a (a, a) }
 
-l :: Map.Map String (String, String) -> String -> String
+type Location = (Char, Char, Char)
+
+l :: Ord a => Map.Map a (a, a) -> a -> a
 l m s = fst $ m Map.! s
 
-r :: Map.Map String (String, String) -> String -> String
+r :: Ord a => Map.Map a (a, a) -> a -> a
 r m s = snd $ m Map.! s
 
 -- Soloutions
 
-steps :: String -> Documents -> Int
-steps "ZZZ" _ = 0
+steps :: Location -> Documents Location -> Int
+steps ('Z', 'Z', 'Z') _ = 0
 steps s Documents{instuctions=(i:is), network=n} = 1 + steps (i n s) (Documents is n)
 steps _ Documents{instuctions=[]} = error "Empty instuctions"
 
 -- Parser
 
-parseDocuments :: String -> Documents
+parseDocuments :: String -> Documents Location
 parseDocuments = parseText documentsParser
 
-documentsParser :: Parser Documents
+documentsParser :: Parser (Documents Location)
 documentsParser = (Documents . cycle <$> many instuctionParser) <*> networkParser
 
-instuctionParser :: Parser (Map.Map String (String, String) -> String -> String)
+instuctionParser :: Ord a => Parser (Map.Map a (a, a) -> a -> a)
 instuctionParser =
     (l <$ symbol "L") <|>
     (r <$ symbol "R")
 
-networkParser :: Parser (Map.Map String (String, String))
+networkParser :: Parser (Map.Map Location (Location, Location))
 networkParser = Map.fromList <$> many nodeParser
 
-nodeParser :: Parser (String, (String, String))
-nodeParser = (,) <$> identifier <*> (symbol "=" *> parens ((,) <$> identifier <*> (comma *> identifier)))
+nodeParser :: Parser (Location, (Location, Location))
+nodeParser = (,) <$> locationParser <*> (symbol "=" *> parens ((,) <$> locationParser <*> (comma *> locationParser)))
+
+locationParser :: Parser Location
+locationParser = lexeme $ (,,) <$> anyChar <*> anyChar <*> anyChar
